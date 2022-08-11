d91a5d40f206492ac7de35ba15994faedf62ba7a
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-11 23:10:28 +0600

    task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character=='A' || character=='E'|| character=='I'|| character=='O'|| character=='U'|| character=='Y' ||
+                character=='a' || character=='e'|| character=='i'|| character=='o'|| character=='u'|| character=='y'){
+            System.out.println("Vowel");
+
+        }else if(((int)character>=65 && (int)character<=90) || ((int)character>=97 && (int)character<=122)){
+            System.out.println("Consonant");
+        }else{
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        /*
+        Basic Salary <= 10000: taxes - 15%.
+        10000 < Basic Salary <= 20000: taxes - 18%
+        Basic Salary > 20000 : taxes - 20%
+        Basic salary < 0 -> "wrong input!"
+         */
+        int taxes = 0;
+        if(salary < 0){
+            System.out.println("wrong input!");
+        }else if(salary <= 10000){
+            taxes = 15;
+            double res = salary - salary * ((double)taxes/100);
+            System.out.println(res);
+        }else if(salary <= 20000 && salary > 10000){
+            taxes = 18;
+            double res = salary - salary * ((double)taxes/100);
+            System.out.println(res);
+        }else{
+            taxes = 20;
+            double res = salary - salary * ((double)taxes/100);
+            System.out.println(res);
+        }
     }
 }
